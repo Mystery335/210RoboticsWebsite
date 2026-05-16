0db393e2397451e60c3b538022cf123f9dc6607f
William-Chen07 <william0714chen@gmail.com>
2026-05-15 22:18:33 -0500

    Deleted unnecessary files and updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -187,15 +187,15 @@ const Home = () => {
             <div className="col-12 col-lg-6">
               <h2 className="section-title">Who We Are</h2>
               <p className="section-text mb-3">
-                FRC Team CRyptonite 624 is a <i>FIRST</i> Robotics Competition
-                team composed of students from Cinco Ranch High School in Katy
-                ISD. We've been building robots since 2001, and have consistently
-                been a top team.
+                210 Robotics is an organization at The University of Texas at 
+                San Antonio competing in both Vex U and the Siemen’s Immersive Design Challenge.
               </p>
-              <h5 className="mb-1">Fun Fact</h5>
-              <p className="section-text">
-                Our team name, "CRyptonite" combines "CR" for Cinco Ranch with
-                "kryptonite"!
+              <p className="section-text mb-3">
+                This organization started Fall 2026, by a group of friends interested in Robotics.               </p>
+              <p className="section-text mb-3">
+                Our goal is to provide a space where students interested in robotics, 
+                programming, or just exploring UTSA can come together and compete in 
+                competitions, participate in socials, and learn new skills. 
               </p>
             </div>
           </div>
