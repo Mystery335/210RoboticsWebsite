578421a4ba99bdb9febea61f09d6633f26383b5f
Mystery335 <pear8423chen@gmail.com>
2026-05-15 21:58:57 -0500

    Changed notfound.css and jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,9 +4,7 @@ export default function NotFound() {
   return (
     <div className="notfound-container">
       <div className="notfound-content">
-        <span className="notfound-number">4</span>
-        <img src="/assets/img/logo-circle.png" alt="404" className="notfound-logo" />
-        <span className="notfound-number">4</span>
+        <img src="/assets/img/210Robotics - Siemens.png" alt="404" className="notfound-logo" />
       </div>
     </div>
   )
