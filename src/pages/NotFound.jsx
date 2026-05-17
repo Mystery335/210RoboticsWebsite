dba455d715c229668a01b90680bf3cdcb30cbbc0
William-Chen07 <william0714chen@gmail.com>
2026-05-17 18:31:05 -0500

    Made some changes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="notfound-container">
       <div className="notfound-content">
-        <img src="/assets/img/210Robotics - Siemens.png" alt="404" className="notfound-logo" />
+        <img src="/assets/img/210Robotics.png" alt="404" className="notfound-logo" />
       </div>
     </div>
   )
