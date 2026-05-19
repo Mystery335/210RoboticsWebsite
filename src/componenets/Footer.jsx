c1922014d3a7c3f87ad06a4de8596d48c00ddaa7
William-Chen07 <william0714chen@gmail.com>
2026-05-18 22:52:49 -0500

    Updated Footer.jsx

--- src/componenets/Footer.jsx
+++ src/componenets/Footer.jsx
@@ -1,13 +1,21 @@
 const Footer = () => {
   return (
     <footer className="footer text-light bg-[#444444]" id="footer">
-      <div className="container py-5">
-        <div className="row g-4">
-          <div className="col-md-4">
-            <p className="mb-2">
+      <div className="container py-1">
+        <div className="row g-0">
+          <div className="col-md-6">
+            <img 
+              src="assets/img/210-Robotics-Transparent-Banner.png" 
+              alt="" 
+              style={{width: '50%', marginLeft: '-3.3%'}}
+            />
+            <p className="mb-2" style={{marginTop: '-5%'}}>
               <i className="fas fa-envelope me-2"></i>
               <a href="mailto:admin@210robotics.com">admin@210robotics.com</a>
             </p>
+          </div>
+
+          <div className="col-md-6 text-end">
             <p className="mb-0">
               <i className="fas fa-map-marker-alt me-2"></i>
               UT San Antonio<br />
@@ -15,16 +23,16 @@ const Footer = () => {
               <span>San Antonio, TX 78249</span>
             </p>
             <div className="social-links mt-3">
-              <a href="https://discord.gg/SddubkhhE" className="text-light text-decoration-none me-2" target = "_blank">
+              <a href="https://discord.gg/SddubkhhE" className="text-light text-decoration-none me-2" target="_blank">
                 <i className="fab fa-discord fa-2x"></i>
               </a>
-              <a href="https://www.instagram.com/210_robotics/" className="text-light text-decoration-none me-2" target = "_blank">
+              <a href="https://www.instagram.com/210_robotics/" className="text-light text-decoration-none me-2" target="_blank">
                 <i className="fab fa-instagram fa-2x"></i>
               </a>
-              <a href="https://www.linkedin.com/company/210-robotics/" className="text-light text-decoration-none me-2" target = "_blank">
+              <a href="https://www.linkedin.com/company/210-robotics/" className="text-light text-decoration-none me-2" target="_blank">
                 <i className="fab fa-linkedin fa-2x"></i>
               </a>
-              <a href="https://github.com/Mystery335/210Robotics_Website" className="text-light text-decoration-none me-2" target = "_blank">
+              <a href="https://github.com/Mystery335/210Robotics_Website" className="text-light text-decoration-none me-2" target="_blank">
                 <i className="fab fa-github fa-2x"></i>
               </a>
             </div>
